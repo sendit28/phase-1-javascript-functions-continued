4a49e01020f040d70fdde0b93bd139f64d6140ee
+ <+>
2022-07-17 18:58:30 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// code your solution here
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork(workPlace = 'go to the office') {
+    return `This Monday, I will ${workPlace}.`
+}
+
+function wrapAdjective(flair = '*') {
+  return (moniker = "special") => {
+        return `You are ${flair}${moniker}${flair}!`
+    }
+}
+
